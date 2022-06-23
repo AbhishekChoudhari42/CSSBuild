157199fa4e7965747fd5a32e176c1f87b7b25db5
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2022-06-23 14:08:22 +0530

    mobile css

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,13 @@
 $ = (element) =>{
     return document.querySelector(element);
 }
+
+
+$('#cancel-button').onclick = function(){
+    $('.popup').classList.remove('show') 
+}
+
+$('.display .copy').onclick = function(){
+    console.log('copy')
+    $('.popup').classList.add('show')
+}
