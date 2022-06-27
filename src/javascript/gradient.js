efbe3349ef6b8266adf87095005619922d2689f8
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2022-06-27 19:58:31 +0530

    gradient

--- src/javascript/gradient.js
+++ src/javascript/gradient.js
@@ -9,9 +9,10 @@ $ = (element) => {
 
 state = {
 
-    colorArray : ['#00ffff','#ff00ff','#fb6309'],
+    colorArray : ['#00ffff','#ff00ff'],
     smooth : 1,
-    angle : 90
+    angle : 90,
+    type : 'linear'
 
 }
 
@@ -23,45 +24,151 @@ function render(){
 
 //  linear-gradient(54deg, rgba(131,58,180,1) 0%, rgba(252,176,69,1) 100%);
     state.colorArray.forEach(element => {
+
         gradient += ','
         gradient += element
          
     });
+    let gradientColor 
     $('.gradient').style.background = 'black'
-    let gradientColor = `linear-gradient(to left${gradient})`;
+    if(state.type == 'linear'){
+         gradientColor = `linear-gradient(${state.angle}deg ${gradient})`;
+
+    }
+    if(state.type == 'radial'){
+         gradientColor = `radial-gradient(closest-side${gradient})`;
+
+    }
+    if(state.type == 'conical'){
+        gradient.slice(0,1)
+        gradientColor = `conical-gradient(${gradient})`;
+
+   }
+    
+    
+    
+    
     $('.gradient').style.background = gradientColor
     $('.gd-background').style.background = gradientColor
 
-    console.log(gradientColor)
+    // console.log(gradientColor)
 
     // adding buttons to color palette
 
     let colorHtml = ''
+    let counter = 0
     state.colorArray.forEach(element =>{
-        colorHtml += `<div class="color" id='cdasd${Math.round(Math.random()*10)}' style = 'background-color : ${element}'  ></div>`
+        colorHtml += `<div class="color" id='${counter}' style = 'background-color : ${element}'  ></div>`
+        counter++
     })
-console.log(colorHtml)
+// console.log(colorHtml)
     $('.color-palette').innerHTML = colorHtml
 
 
+
+    $('#angleOutput').innerHTML =  state.angle + 'deg'
+    select();
+
+
+
+    document.querySelectorAll('.type button').forEach(element=>{
+        if(element.id == state.type){
+            $(`#${element.id}`).classList.add('active')
+
+        }else{
+            $(`#${element.id}`).classList.remove('active')
+
+        }
+    })
      
 
 }
 render();
 
+
+
+
+// add new color
+
 $('#color-add').onclick = function(){
     console.log('colornew')
     $('.color-controls').classList.toggle('display-color-controls');  
     $('#color-add span').classList.toggle('cross-show');
+    $('.color-change').classList.remove('display-color-change')
+
 }
 
 $('#add-new-color').onclick = function(){
     console.log('colornewadded')
     $('.color-controls').classList.remove('display-color-controls');  
+    $('#color-add span').classList.toggle('cross-show');
+    let newColor = $('#newColor').value
+    console.log(newColor)
+
+    state.colorArray.push(`${newColor}`)
+    render();
+}
+    
+   
+
+
+// angle
+
+$('#angle').oninput = function(){
+    state.angle =  $('#angle').value
+    // state.copy = 'copy';
+    render();
 }
 
 
+// change or delete existing color
+let colorID = ''
+function select(){
+document.querySelectorAll('.color').forEach(element=>{
+    element.onclick = function(e){
+        colorID=  e.target.id
+        console.log(colorID)
+       
+        $('.color-controls').classList.remove('display-color-controls');  
+        $('#color-add span').classList.remove('cross-show');
 
-$('.color').onclick = function(e){
-    console.log(e.target.id)
+        $('.color-change').classList.toggle('display-color-change')
+        
+        $('#change-color').onclick = function(){
+            let color = $('#changed-color').value
+            state.colorArray[colorID] = `${color}`
+        $('.color-change').classList.remove('display-color-change')
+
+            render()
+        } 
+        $('#remove').onclick = function(){
+            if(state.colorArray.length > 2){
+            state.colorArray.splice(colorID, 1);
+            $('.color-change').classList.remove('display-color-change')
+
+            render()
+            }
+            else{
+                $('#remove').classList.add('wobble')
+            }
+            setTimeout(() => {
+                $('#remove').classList.remove('wobble')
+            }, 2000);
+        } 
+        
+
+}})}
+
+$('.up').onclick = function(){
+    $('.color-change').classList.toggle('display-color-change')
+
 }
+
+
+document.querySelectorAll('.type button').forEach(element=>{
+   element.onclick = function(){
+    state.type = element.id
+    console.log(element.id)
+    render()
+   }
+})
