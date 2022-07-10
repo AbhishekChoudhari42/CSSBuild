d3316ed46ef8b0f2b7f358ce23d1aeb557b6bf8c
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2022-07-10 12:28:39 +0530

    nav bar

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,12 +2,14 @@ $ = (element) =>{
     return document.querySelector(element);
 }
 
-
+if($('#cancel-button')){
 $('#cancel-button').onclick = function(){
     $('.popup').classList.remove('show') 
 }
+}
 
+if($('.code')){
 $('.code').onclick = function(){
     
     $('.popup').classList.add('show')
-}
+}}
